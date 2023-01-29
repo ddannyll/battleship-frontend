efa702142911d72d6568fb075d22c775b2566d4d
Daniel Nguyen <daniel.nguyen.s173@gmail.com>
2023-01-29 19:08:29 +1100

    basic ship placement implemented

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,46 +1,136 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import PlayerBoard from "./PlayerBoard"
+import { difference } from "lodash"
+
+const SHIP_NAMES = [
+    'carrier',
+    'battleship',
+    'submarine',
+    'destroyer',
+    'patrolBoat',
+]
+
 
 export default function Game(props) {
     const { id } = useParams()
-    const [ token, setToken ] = useState(null)
     const { backendUrl } = props
-    const [ response, setResponse ] = useState('{}')
+    const [ token, setToken ] = useState()
+    const [ board, setBoard ] = useState()
+    const [ enemyBoard, setEnemyBoard ] = useState()
+    const [ state, setState ] = useState()
+    const [ error, setError ] = useState(null)
+    const [ playerClickCell, setPlayerClickCell ] = useState(() => {})
+    const [ shipsToPlace, setShipsToPlace ] = useState([])
 
-    useEffect(() => {
-        const getUrl = new URL(backendUrl)
-        getUrl.pathname = `response/${id}`
-        getUrl.searchParams.set('token', token)
-        fetch(getUrl, {
-            mode:'cors',
-            method:'GET'
-        })
-        .then(response => {
+    const handleResponse = (response) => {
+        response.then(response => {
             if (!response.ok) {
-                throw new Error(response.statusText)
+                return response.text().then(text=>{throw new Error(text)})
             }
             return response.json()
         })
         .then(response => {
             setToken(response.token)
-            setResponse(JSON.stringify(response))
+            setBoard(response.board)
+            setEnemyBoard(response.enemyBoard)
+            setState(response.state)
+            setError(null)
+            setShipsToPlace(difference(SHIP_NAMES, Object.keys(response.board.ships)))
         })
         .catch(err => {
-            console.error(err);
+            setError(err.message)
+            console.error(err.message);
         })
-    }, [])
+    }
 
-    const responseObj = JSON.parse(response)
-    const { board, enemyBoard, attackTurn, state, winner } = responseObj
+    const fetchData = useCallback(() => {
+        console.log('fetch')
+        const getUrl = new URL(backendUrl)
+        getUrl.pathname = `response/${id}`
+        getUrl.searchParams.set('token', token)
+        handleResponse(
+            fetch(getUrl, {
+                mode:'cors',
+                method:'GET'
+            })
+        )
+    }, [id, backendUrl, token])
 
-    console.log(responseObj);
+    const placeShip = useCallback((shipName, x, y, vertical) => {
+        const postUrl = new URL(backendUrl)
+        postUrl.pathname = `place`
+        handleResponse(
+            fetch(postUrl, {
+                mode:'cors',
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    shipName,
+                    playerId: 1,
+                    position: {
+                        x,
+                        y
+                    },
+                    vertical
+                })
+            })
+        )
+        console.log('placing ship: ' + shipName);
+    }, [backendUrl, token])
+
+    const attack = (x, y) => {
+        const postUrl = new URL(backendUrl)
+        postUrl.pathname = `attack/${id}`
+        handleResponse(
+            fetch(postUrl, {
+                mode:'cors',
+                method:'POST',
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    token,
+                    position: {
+                        x,
+                        y
+                    }
+                })
+            })
+        )
+    }
+
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
+
+    useEffect(() => {
+        switch (state) {
+            case "place":
+                if (shipsToPlace.length > 0) {
+                    setPlayerClickCell(() => (x, y) => {placeShip(shipsToPlace[0], x, y, false)})
+                } else {
+                    setPlayerClickCell(() => (() => {}))
+                }
+                break;
+            case "battle":
+                break
+            case "finish":
+
+                break
+            default:
+
+                break;
+        }
+    }, [placeShip, shipsToPlace, state])
+
+
 
 
     return (
         <>
         Game {id} {token}
-        <PlayerBoard board={board}/>
+        {error}
+        <PlayerBoard board={board} clickCell={playerClickCell}/>
         </>
     )
 }
--- src/Components/PlayerBoard.js
+++ src/Components/PlayerBoard.js
@@ -1,25 +1,42 @@
+import './PlayerBoard.css'
+
+import { isEqual } from 'lodash'
+
 const BOARD_SIZE = 10
 
-export default function PlayerBoard({board}) {
+export default function PlayerBoard({board, clickCell}) {
+    console.log(board);
+    if (!board) {
+        return <></>
+    }
+
+    const {ships, shells} = board
+
+    const shipPositions = []
+        Object.values(ships).forEach(ship => {
+        ship.positions.forEach(position => shipPositions.push(position))
+    })
+    
+    
     const cells = []
     for (let i = 0; i < BOARD_SIZE; i++) {
         for (let j = 0; j < BOARD_SIZE; j++) {
             cells.push(
-                <div 
-                    data-x={j} 
-                    data-y={i} 
+                <div
                     key={`${j}${i}`} 
-                    onClick={(e) => {}
-                }>
-                    test
+                    onClick={(e) => {clickCell(j, i)}}
+                    className={shipPositions.some(position => isEqual(position, {x: j, y:i}))  ?
+                    'cell s' :
+                    'cell w'}
+                >
                 </div>)
         }
     }
 
-    console.log(cells);
+
     return (
-        <>
+        <div className="board">
             {cells}
-        </>    
+        </div>    
     )
 }
